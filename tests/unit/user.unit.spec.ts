f15c3242654610468246836ba6a5d7a76031066f
Vladislav Syomkin <bloodhomaq@gmail.com>
2022-11-22 15:00:26 +0300

    added tests

--- tests/unit/user.unit.spec.ts
+++ tests/unit/user.unit.spec.ts
@@ -5,33 +5,17 @@ import BcryptModule from '../../src/application/di/modules/bcrypt.module';
 import UserDto from '../../src/application/http/dtos/user.dto';
 import UserRepository from '../../src/data/postgres/repositories/user.repository';
 import User from '../../src/domain/entities/user.entity';
-import SexEnum from '../../src/domain/enums/sex.enum';
-import IUserCreate from '../../src/domain/interfaces/user-create.interface';
 import UserService from '../../src/domain/use-cases/user.service';
+import {
+  USER_MOCK,
+  USER_MOCK_SCHEMA,
+  USER_REPOSITORY_CREATE_FN_MOCK,
+  USER_RESPONSE_MOCK,
+  USERS_REPOSITORY_MOCK,
+} from './user.mock';
 
 describe('User Service', () => {
   let userService: UserService;
-
-  const usersRepositoryMock = {
-    findOneById: jest.fn(),
-    findOne: jest.fn(),
-    findMany: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(),
-    softDeleteById: jest.fn(),
-  };
-
-  const testUserData = {
-    login: 'test',
-    password: 'test',
-    email: 'test@test.com',
-    firstName: 'test',
-    lastName: 'test',
-    middleName: 'test',
-    phoneNumber: 'test',
-    sex: SexEnum.MALE,
-  };
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -40,7 +24,7 @@ describe('User Service', () => {
         UserService,
         {
           provide: UserRepository,
-          useValue: usersRepositoryMock,
+          useValue: USERS_REPOSITORY_MOCK,
         },
       ],
     }).compile();
@@ -49,90 +33,65 @@ describe('User Service', () => {
   });
 
   describe('User Service', () => {
-    it('Создание пользователя', async () => {
-      usersRepositoryMock.create = jest.fn().mockImplementationOnce(
-        ({
-          login,
-          password,
-          email,
-          firstName,
-          lastName,
-          middleName,
-          phoneNumber,
-          sex,
-        }: IUserCreate): User =>
-          new User({
-            id: 1,
-            login,
-            email,
-            firstName,
-            lastName,
-            middleName,
-            phoneNumber,
-            sex,
-            password,
-          }),
-      );
+    it('Создание пользователя - types', async () => {
+      USERS_REPOSITORY_MOCK.create = USER_REPOSITORY_CREATE_FN_MOCK;
 
-      const result = {
-        id: 1,
-        login: 'test',
-        email: 'test@test.com',
-        firstName: 'test',
-        lastName: 'test',
-        middleName: 'test',
-        phoneNumber: 'test',
-        sex: SexEnum.MALE,
-      };
+      const expectedResult = await userService.create(USER_MOCK);
 
-      const user = await userService.create(testUserData);
-
-      expect(user?.id).toEqual(1);
-      expect(user?.password).not.toEqual(testUserData.password);
-
-      const cleanedUserData = plainToInstance(UserDto, user);
-
-      expect(cleanedUserData).toEqual(result);
+      expect(expectedResult).toMatchObject(USER_MOCK_SCHEMA);
     });
 
-    it('Поиск пользователя - success', async () => {
-      usersRepositoryMock.findOneById = jest.fn().mockImplementationOnce(
+    it('Создание пользователя - data', async () => {
+      USERS_REPOSITORY_MOCK.create = USER_REPOSITORY_CREATE_FN_MOCK;
+
+      const user = await userService.create(USER_MOCK);
+
+      expect(user?.id).toEqual(1);
+      expect(user?.password).not.toEqual(USER_MOCK.password);
+
+      const expectedResult = plainToInstance(UserDto, user);
+
+      expect(expectedResult).toEqual(USER_RESPONSE_MOCK);
+    });
+
+    it('Поиск пользователя - types', async () => {
+      USERS_REPOSITORY_MOCK.findOneById = jest.fn().mockImplementationOnce(
         (id: number): User =>
           new User({
             id,
-            ...testUserData,
+            ...USER_MOCK,
           }),
       );
 
-      const result = {
-        id: 1,
-        login: 'test',
-        password: 'test',
-        email: 'test@test.com',
-        firstName: 'test',
-        lastName: 'test',
-        middleName: 'test',
-        phoneNumber: 'test',
-        sex: SexEnum.MALE,
-      };
+      const expectedResult = await userService.findOneById(USER_MOCK.id);
 
-      const user = await userService.findOneById(1);
+      expect(expectedResult).toMatchObject(USER_MOCK_SCHEMA);
+    });
 
-      expect(user).toEqual(result);
+    it('Поиск пользователя - data', async () => {
+      USERS_REPOSITORY_MOCK.findOneById = jest.fn().mockImplementationOnce(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (id: number): User =>
+          new User({
+            ...USER_MOCK,
+          }),
+      );
+
+      const expectedResult = await userService.findOneById(USER_MOCK.id);
+
+      expect(expectedResult).toEqual(USER_MOCK);
     });
 
     it('Поиск пользователя - failed', async () => {
-      usersRepositoryMock.findOneById = jest
+      USERS_REPOSITORY_MOCK.findOneById = jest
         .fn()
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         .mockImplementationOnce((id: number): User => undefined);
 
-      const id = 1;
-
       try {
-        await userService.findOneById(id);
-      } catch (err) {
-        expect(err.message).toBe(`Пользователь с id = ${id} не найден`);
+        await userService.findOneById(USER_MOCK.id);
+      } catch (expectedError) {
+        expect(expectedError.message).toBe(`Пользователь с id = ${USER_MOCK.id} не найден`);
       }
     });
   });
